d1b34225c66bb2a71c5842f67afc2acb49a28a85
Sebastian <sebastianberiio@gmail.com>
2024-06-25 23:33:59 -0500

    fixing_mifare_classic 001132

--- smartCard/AcsReader.cpp
+++ smartCard/AcsReader.cpp
@@ -337,22 +337,21 @@ ApduResponse AcsReader::parseResponse(char *response, ulong responseLength, bool
     qDebug() << "responseLength:" << responseLength;
     qDebug() << "response data:" << QByteArray::fromRawData(response, responseLength).toHex();
 
-    if (responseLength >= 2)
-    {
-        uint8_t highByte = static_cast<uint8_t>(response[responseLength - 2]);
-        uint8_t lowByte = static_cast<uint8_t>(response[responseLength - 1]);
-        apduResponse.statusWord = (highByte << 8) | lowByte;
-        apduResponse.data = QByteArray(response, responseLength - 2); // Exclude the status word bytes from the data
-
-        qDebug() << "Extracted statusWord for Mifare Classic (hex):" << QString::number(apduResponse.statusWord, 16).toUpper();
-    }
-
-    else if (mplus && responseLength >= 1)
+    if (mplus && responseLength >= 1)
     {
         apduResponse.statusWord = (response[0] << 8) | 0x00;
         apduResponse.data = QByteArray::fromRawData(response, responseLength);
 
         qDebug() << "Extracted statusWord for Mifare Plus (hex):" << QString::number(apduResponse.statusWord, 16).toUpper();
+    }
+    if (responseLength >= 2)
+    {
+        uint8_t highByte = static_cast<uint8_t>(response[responseLength - 2]);
+        uint8_t lowByte = static_cast<uint8_t>(response[responseLength - 1]);
+        apduResponse.statusWord = (highByte << 8) | lowByte;
+        apduResponse.data = QByteArray(response, responseLength);
+
+        qDebug() << "Extracted statusWord (hex):" << QString::number(apduResponse.statusWord, 16).toUpper();
     }
 
     else
