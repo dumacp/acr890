70f0f8218523aeb0fd4f1697fd09f596146918c6
Sebastian <sebastianberiio@gmail.com>
2024-06-25 23:27:31 -0500

    fixing_mifare_classic 001130

--- smartCard/AcsReader.cpp
+++ smartCard/AcsReader.cpp
@@ -344,7 +344,7 @@ ApduResponse AcsReader::parseResponse(char *response, ulong responseLength, bool
 
         qDebug() << "Extracted statusWord for Mifare Plus (hex):" << QString::number(apduResponse.statusWord, 16).toUpper();
     }
-    else if (!mplus && responseLength >= 2)
+    else if (responseLength >= 2)
     {
         uint8_t highByte = static_cast<uint8_t>(response[responseLength - 2]);
         uint8_t lowByte = static_cast<uint8_t>(response[responseLength - 1]);
