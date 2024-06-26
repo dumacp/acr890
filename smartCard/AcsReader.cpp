ea395f841060aa59726ecf6e995b68f65d943306
Sebastian <sebastianberiio@gmail.com>
2024-06-25 22:43:56 -0500

    fixing_mifare_classic 001120

--- smartCard/AcsReader.cpp
+++ smartCard/AcsReader.cpp
@@ -340,10 +340,11 @@ ApduResponse AcsReader::parseResponse(char *response, ulong responseLength, bool
     if (mplus && responseLength >= 1)
     {
         apduResponse.statusWord = (response[0] << 8) | 0x00;
-        apduResponse.data = QByteArray::fromRawData(response, responseLength);
+        /*    apduResponse.data = QByteArray::fromRawData(response, responseLength); */
+        apduResponse.data = QByteArray(response, responseLength);
     }
 
-    if (responseLength >= 2)
+    else if (responseLength >= 2)
     {
         uint8_t highByte = static_cast<uint8_t>(response[responseLength - 2]);
         uint8_t lowByte = static_cast<uint8_t>(response[responseLength - 1]);
